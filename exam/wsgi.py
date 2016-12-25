b5e5fd5ead49e3bacab213e9ea8d7fa90e269366
Tim <oneuptim@Tims-MacBook-Pro.local>
2016-12-25 11:27:57 -0800

    Updated wsgi

--- exam/wsgi.py
+++ exam/wsgi.py
@@ -11,8 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exam.settings")
+
+from dj_static import Cling
+application = Cling(get_wsgi_application()
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
